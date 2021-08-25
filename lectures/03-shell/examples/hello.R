67e07b7e4f2f030e1a832c644931f06c729a50bc
Davide Proserpio <proserpi@marshall.usc.edu>
2021-08-24 18:14:03 -0700

    polished lecture 3

--- lectures/03-shell/examples/hello.R
+++ lectures/03-shell/examples/hello.R
@@ -2,4 +2,4 @@ args = commandArgs(trailingOnly = TRUE)
 i = args[1]; j = args[2]
 
 cat('Hello World, from R!\n', 
-     i, '+', j, '=', as.integer(i) + as.integer(j))
+     i, '+', j, '=', as.integer(i) + as.integer(j),'\n')
